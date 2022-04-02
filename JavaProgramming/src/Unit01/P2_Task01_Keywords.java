882c121732d07b0e7d3b89bf86a6f01210c5981b
user <user@LAPTOP-5MD64LG2>
2022-04-02 16:33:53 +0530

    hello

---
+++ JavaProgramming/src/Unit01/P2_Task01_Keywords.java
@@ -0,0 +1,23 @@
+package Unit01;
+
+public class P2_Task01_Keywords {
+
+	public static void main(String[] args) {
+		/*keywords are reserved words in java that has some special meaning and that cannot be changed.
+		 * In Java we have 50 keywords .
+		 * Some of these are -abstract ,assert,boolean ,boolean,case,double etc
+		 * Some keywords are meant for future use of only and some are supported by specific version of java
+		 */
+		
+       int double =1;
+       int new()
+       {
+    	   System.out.println("empty");
+    	   
+       }
+       int a =10;
+       a=11;//value gets changed
+	}
+
+}
+}
